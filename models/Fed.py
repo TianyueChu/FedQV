10f9137aa2e7690e8728707591b2ebcf70f1d7f3
Tianyue Chu <30310264+TianyueChu@users.noreply.github.com>
2022-08-11 18:41:42 +0200

    Update Fed.py

--- models/Fed.py
+++ models/Fed.py
@@ -17,7 +17,7 @@ from sklearn.preprocessing import Normalizer
 
 eps = np.finfo(float).eps
 
-def aggregate(args,w_local,n,w_global,vote_budget):
+def aggregate(args,w_local,n,w_global,vote_budget,global_acc):
     if args.agg == 'fedavg':
         print("using FedAvg")
         w_avg = FedAvg(w_local,n)
@@ -26,13 +26,19 @@ def aggregate(args,w_local,n,w_global,vote_budget):
         w_avg = FedQ(w_local,n)
     elif args.agg == 'fedqv':
         print("using FedQV Estimator")
-        w_avg, vote_budget = FedQV(w_local,w_global,vote_budget,n,args.rep)
+        w_avg, vote_budget = FedQV(w_local,w_global,vote_budget,n,args.rep,args.theta,global_acc)
     elif args.agg == 'krum':
         print("using Krum Estimator")
         w_avg = Krum(w_local,w_global)
+    elif args.agg == 'trimmed-mean':
+        print("using trimmed mean Estimator")
+        w_avg = trimmed_mean(w_locals, trim_ratio = args.num_attackers/args.num_users )
     elif args.agg == 'multi-krum':
         print("using Multi-Krum Estimator")
-        w_avg = Mkrum(w_local,w_global, b= 0.5* args.num_users*args.frac)
+        w_avg, vote_budget = Mkrum(w_local,w_global,vote_budget,n, args.qv, args.rep, b= 0.1 * args.num_users* args.frac)
+    elif args.agg == 'median':
+        print("using median Estimator")
+        w_avg = Median(w_locals)
     else:
         exit('Error: unrecognized aggregation method')
     return w_avg, vote_budget
@@ -75,10 +81,10 @@ def Krum(w,w_global):
         dists = torch.tensor(dists)
         scores.append(dists[sorted_index[0]].sum())
     ind_min = scores.index(min(scores))
-    print('using updates from worker ', ind_min)
+    print('using updates from voter index ', ind_min)
     return w[ind_min]
 
-def Mkrum(w,w_global,b):
+def Mkrum(w,w_global,vote_budget,n,qv_on,reputation_on,b):
     
     num_clients = len(w)
  
@@ -112,38 +118,41 @@ def Mkrum(w,w_global,b):
         for k in range(len(sorted_index)):
             ind_dict[str(i) +','+ str(sorted_index[k].numpy()+i+1)] = dists[sorted_index[k]]
             scores.append(dists[sorted_index[k]])
-    # print(scores)
+
     res = torch.sort(torch.tensor(scores), descending=False)[:num_selection]
-    # print('using updates from worker ', ind_min)
-    # print(ind_dict)
+
     keys=list(ind_dict.keys()) 
     values=list(ind_dict.values())
     ind_list = []
     
     for i in res[0][0:num_selection+1].numpy():
         ind_list = ind_list + list(map(int, keys[values.index(i)].rsplit(',')))
-    # print(ind_list)
+
     select_list = []
     [select_list.append(v) for v in ind_list if v not in select_list]
     final_list = select_list[:num_selection]
+    final_list.sort()
     print('worker index', final_list)
     
     #### aggregation part
-    ### without QV
-    w_avg = copy.deepcopy(w[final_list[0]])
-    for k in w_avg.keys():
-        w_avg[k] = torch.mul(w[final_list[0]][k],1/len(final_list))
-        for i in final_list[1:]:
-            w_avg[k] += torch.mul(w[i][k],1/len(final_list))
-        
-    ### if using QV
-    # for k in w_avg.keys():
-       # w_avg[k] = torch.mul(w[final_list[0]][k],n[final_list[0]]/sum(n))
-        # for i in final_list[1:]:
-            # print(i)
-            # w_avg[k] += torch.mul(w[i][k],n[i]/sum(n))
-            
-    return w_avg
+    ### with QV
+    if qv_on:
+        print("Plugging FedQV")
+        w_qv = [w[i] for i in final_list]
+        vote_budget_qv = [vote_budget[i] for i in final_list]
+        n_qv = [n[i] for i in final_list]
+        w_avg, vote_budget_qv = FedQV(w_qv, w_global, vote_budget_qv, n_qv, reputation_on=reputation_on,theta=0) 
+        for idx, i in enumerate(final_list):
+                vote_budget[i] = vote_budget_qv[idx]
+    
+    else:
+        w_avg = copy.deepcopy(w[final_list[0]])
+        for k in w_avg.keys():
+            w_avg[k] = torch.mul(w[final_list[0]][k],1/len(final_list))
+            for i in final_list[1:]:
+                w_avg[k] += torch.mul(w[i][k],1/len(final_list))
+    
+    return w_avg,vote_budget
     
 
 def FedQ(w,n):
@@ -157,114 +166,120 @@ def FedQ(w,n):
             w_avg[k] += torch.mul(w[i][k],math.sqrt(n[i])/sum_weight)
     return w_avg
 
-def FedQV(w, w_global, vote_budget,n, reputation_on):
+def FedQV(w, w_global, vote_budget, n, reputation_on, theta, acc):
     # type(w): list
     # type(w_global): OrderedDict
     # type(w[0]): OrderedDict
     # len(w[0]): how many layers in NN
+    
+    w, invalid_model_idx = get_valid_models(w)
+    print("invalid model",invalid_model_idx)
+    
     num_clients = len(w)
-    glob_grad = []
     
-    for j in list(w_global.values()):
-        glob_grad = np.append(glob_grad, np.array(j.tolist()).flatten())
-        # print(glob_grad.shape)
-        # glob_grad += np.array(j.tolist()).reshape(1,-1).tolist()
+    if len(invalid_model_idx) == num_clients:
+        w_avg = copy.deepcopy(w_global)
+        for k in w_avg.keys():
+            w_avg[k] = w_global[k]
+            print("no valid model")
+    else:
+        for i in sorted(invalid_model_idx, reverse=True):
+            del vote_budget [i]
+    
         
-    grad_len = len(glob_grad)
-    grads = np.zeros((num_clients, grad_len))
-    weight = []
-    
-    for i in range(num_clients):
-        layer = []
-        for j in list(w[i].values()):
-            layer = np.append(layer, np.array(j.tolist()).flatten())
-            # print(layer.shape)
-        # grads[i] contains all the grads of client[i]
-        grads[i] = layer
-        weight.append(smp.cosine_similarity(grads[i].reshape(1,-1), glob_grad.reshape(1,-1)).flatten().tolist()[-1])
-        # print('voice credit:',weight[-1])
-    # print(weight)
-    
-    # Normalization
-    # https://towardsdatascience.com/scale-standardize-or-normalize-with-scikit-learn-6ccc7d176a02
-    # normalized_weight = Normalizer().fit_transform(np.array(weight))
-    # normalized_weight = preprocessing.normalize([np.array(weight)])
-    
-    # Case 1: normalization
-    scaler = MinMaxScaler()
-    normalized_weight = scaler.fit_transform(np.array(weight).reshape(-1,1))
-    # print(normalized_weight)
-    voice_credit = []
-    for i in range(len(normalized_weight)):
-       if normalized_weight[i][0] in [0,1]:
-          voice_credit.append(0)
-       else:
-          if -math.log(normalized_weight[i][0]) >= 0 :
-              voice_credit.append(-math.log(normalized_weight[i][0]))
-          else:
-              voice_credit.append(0)
-              print('abnormal weight is', normalized_weight[i][0])
-    
-    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-    # Case 2: without normalization
-    # normalized_weight = weight
-    # weight_sum = sum(weight)
-    # for i in range(len(normalized_weight)):
-        # normalized_weight[i] = normalized_weight[i]/weight_sum
-    
-    # print(normalized_weight)    
-    # voice_credit = []
-    # for i in range(len(normalized_weight)):
-       # if normalized_weight[i] in [0,1]:
-           # voice_credit.append(0)
-       # else:
-           # if -math.log(normalized_weight[i]) >= 0 :
-               # voice_credit.append(-math.log(normalized_weight[i]))
-           # else:
-               # voice_credit.append(0)
-               # print('abnormal weight is', normalized_weight[i]) 
-
-    # print('voice_credit', voice_credit, 'before budget')
+        glob_grad = []
+        w_avg = copy.deepcopy(w[0])
         
-    if reputation_on:
-        rep_score = reputation_aggregation(w, LAMBDA=2, thresh=0.05)
-        for i in range(len(vote_budget)):
-            if rep_score[i] > statistics.median(rep_score):
-                vote_budget[i] += rep_score[i]
-                print('voter',i,'increase budget',rep_score[i])
+        for j in list(w_global.values()):
+            glob_grad = np.append(glob_grad, np.array(j.tolist()).flatten())
+        
+        grad_len = len(glob_grad)
+        grads = np.zeros((num_clients, grad_len))
+        weight = []
+        
+        for i in range(num_clients):
+            layer = []
+            for j in list(w[i].values()):
+                layer = np.append(layer, np.array(j.tolist()).flatten())
                 
-    # voice credit budget
-    for i in range(len(vote_budget)):
-        if vote_budget[i] == 0:
-            voice_credit[i] = 0
-            # print('voter {} budget run out'.format(i))
-        elif vote_budget[i]- voice_credit[i] <= 0:
-            voice_credit[i] = vote_budget[i]
-            vote_budget[i] = 0
-            # print('vote budget run out to', vote_budget[i] , 'voice credit is', voice_credit[i] )
-        elif vote_budget[i]- voice_credit[i] > 0:
-            vote_budget[i] = vote_budget[i]-voice_credit[i]
-            # print('vote budget is', vote_budget[i] , 'voice credit is', voice_credit[i] )
-        else:
-            print('Error')
-    
-    print('After budget, voice_credit', voice_credit)
-    
-    voice_credit = [a*b for a,b in zip(voice_credit,n)]
-    
-    w_avg = copy.deepcopy(w[0])
-    sum_weight = sum(np.sqrt(voice_credit))
-    agg_weight = []
-    for i in range(len(voice_credit)):
-        agg_weight.append(math.sqrt(voice_credit[i])/sum_weight)
-    print('aggregation weights are', agg_weight)
+            # grads[i] contains all the grads of client[i]        
+            grads[i] = layer
+            sim_score = smp.cosine_similarity(grads[i].reshape(1,-1), glob_grad.reshape(1,-1)).flatten().tolist()[-1]
+            weight.append(sim_score)
         
-    # k: layer in NN
-    for k in w_avg.keys():
-        w_avg[k] = torch.mul(w[0][k],math.sqrt(voice_credit[0])/sum_weight)
-        # len(w): num_clients
-        for i in range(1, len(w)):
-            w_avg[k] += torch.mul(w[i][k],math.sqrt(voice_credit[i])/sum_weight)
+          
+        # Normalization
+        scaler = MinMaxScaler()
+        normalized_weight = scaler.fit_transform(np.array(weight).reshape(-1,1))
+        # voice credit
+        voice_credit = []
+        for i in range(len(normalized_weight)):
+            if normalized_weight[i][0] <= theta:
+                voice_credit.append(0)
+                if normalized_weight[i][0] == 0:
+                    vote_budget[i] = 0
+                else:
+                    vote_budget[i] = max(vote_budget[i] + (math.log(normalized_weight[i][0]) - 1), 0)
+            elif normalized_weight[i][0] >= 1 - theta:
+                voice_credit.append(0)
+                vote_budget[i] = max(vote_budget[i] + (math.log(normalized_weight[i][0]) - 1), 0)
+            else:
+                voice_credit.append(-math.log(normalized_weight[i][0]) + 1)
+        
+       # Reputation model
+        if reputation_on:
+            w, rep_score = reputation_aggregation(w, LAMBDA=2, thresh=0.05)
+            for i in range(len(vote_budget)):
+                if rep_score[i].cpu().numpy() > 0.9:
+                    vote_budget[i] += rep_score[i].cpu().numpy()
+                    voice_credit[i] += rep_score[i].cpu().numpy()
+                    print('index',i,'increase budget',rep_score[i].cpu().numpy())
+                elif rep_score[i].cpu().numpy()== 0:
+                    vote_budget[i] = 0
+                    voice_credit[i] = 0
+                    # print('voter',i,'increase budget',rep_score[i])    
+        # voice credit budget
+       
+        for i in range(len(vote_budget)):
+            # budget already run out
+            if vote_budget[i] <= 0:
+                voice_credit[i] = 0
+                vote_budget[i] = 0
+            # vote budget run out this time
+            elif vote_budget[i]- voice_credit[i] <= 0:
+                voice_credit[i] = vote_budget[i]
+                vote_budget[i] = 0
+            elif vote_budget[i] - voice_credit[i] > 0:
+                vote_budget[i] = vote_budget[i]-voice_credit[i]
+            else:
+                print('Error')
+        
+        # caculate sum of the voice credits
+        voice_credit = [a*b for a,b in zip(voice_credit,n)]
+        voice_credit = [i if i >= 0 else 0 for i in voice_credit]
+        sum_weight = sum(np.sqrt(voice_credit))
+ 
+        
+        agg_weight = []
+        for i in range(len(voice_credit)):
+            agg_weight.append(math.sqrt(voice_credit[i])/sum_weight)
+        
+        if np.isnan(agg_weight).any():
+            for k in w_avg.keys():
+                w_avg[k] = w_global[k]
+            print("using the global model")
+        else:        
+            print('aggregation weights are', agg_weight)
+            # k: layer in NN
+            for k in w_avg.keys():
+                w_avg[k] = torch.mul(w[0][k],math.sqrt(voice_credit[0])/sum_weight)
+                # len(w): num_clients
+                for i in range(1, len(w)):
+                    w_avg[k] += torch.mul(w[i][k],math.sqrt(voice_credit[i])/sum_weight)
+        
+        for i in sorted(invalid_model_idx,reverse=False):
+            vote_budget.insert(i,0)
+            
     return w_avg, vote_budget
     
     
@@ -391,7 +406,7 @@ def reputation_aggregation(w_locals, LAMBDA, thresh):
     normalized_rep_score = torch.tensor(normalized_rep_score.reshape(1,-1)[0])
     print('reputation after normalized', normalized_rep_score)
     
-    return normalized_rep_score
+    return w, normalized_rep_score
 
 def is_valid_model(w):
     if isinstance(w, list):
@@ -452,3 +467,44 @@ def repeated_median(y):
 
     return slopes, intercepts
     
+    
+def trimmed_mean(w, trim_ratio):
+    trim_num = int(trim_ratio * len(w))
+    device = w[0][list(w[0].keys())[0]].device
+    w_med = copy.deepcopy(w[0])
+    for k in w_med.keys():
+        shape = w_med[k].shape
+        if len(shape) == 0:
+            continue
+        total_num = reduce(lambda x, y: x * y, shape)
+        y_list = torch.FloatTensor(len(w), total_num).to(device)
+        for i in range(len(w)):
+            y_list[i] = torch.reshape(w[i][k], (-1,))
+        y = torch.t(y_list)
+        y_sorted = y.sort()[0]
+        result = y_sorted[:, trim_num:-trim_num]
+        result = result.mean(dim=-1)
+        assert total_num == len(result)
+
+        weight = torch.reshape(result, shape)
+        w_med[k] = weight
+    return w_med
+    
+def Median(w):
+    device = w[0][list(w[0].keys())[0]].device
+    w_med = copy.deepcopy(w[0])
+    for k in w_med.keys():
+        shape = w_med[k].shape
+        if len(shape) == 0:
+            continue
+        total_num = reduce(lambda x, y: x * y, shape)
+        y_list = torch.FloatTensor(len(w), total_num).to(device)
+        for i in range(len(w)):
+            y_list[i] = torch.reshape(w[i][k], (-1,))
+        y = torch.t(y_list)
+        median_result = median_opt(y)
+        assert total_num == len(median_result)
+
+        weight = torch.reshape(median_result, shape)
+        w_med[k] = weight
+    return w_med
